80269309bd0b6cd5b7b36e02fe278bdac238a207
dawoud-alhendi <dawoudstar@gmail.com>
2023-09-13 05:55:49 +0300

    my task

--- index.js
+++ index.js
@@ -8,4 +8,41 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+let heightInmeter = height/100;
+let heightDubble = heightInmeter^2;
+let bmi = weight/heightDubble;
+//alert(bmi)
+if (bmi <18.5){
+  alert(`${bmi}\n under weight`)
+ 
+}
+else if (bmi > 18.5 && bmi < 24.9){
+  alert(`healthy weight\n ${bmi}`);
+}
+else if (bmi >=25.0 && bmi <29.9){
+  alert(`over weight \n${bmi}`);
+}
+else if (bmi >30){
+  alert(`above obesity\n${bmi}`);
+}
+
+if (age >=19 && age <= 24 && bmi >=19 && bmi <= 24) {
+
+  alert("normal bmi")
+}
+else if (age >=25 && age <=34 && bmi >=20 && bmi <= 24){
+  alert("normal bmi ")
+}
+else if (age >=35 && age  <=44 && bmi >=21 && bmi <=26){
+  alert("normal bmi")
+}
+else if (age >=45 && age  <=54 && bmi >=22 && bmi <=27)
+  alert(" narmal")
+}
+else if (age >=55 && age  <=64 && bmi >=23 && bmi <=28){
+  alert(" narmal")
+}
+else if (age >= 65 && bmi >= 24 && bmi <= 29 ){
+
+ alert("narmal")
 }
